1091ecaa6d797b3da206bdb3bc24e934ec202d30
nourhana15 <nourhana.a1996@gmail.com>
2025-04-28 22:44:04 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,45 @@
+
+## String
+name = "Nourhan"
+print(name)
+print(type(name))
+
+#### Numerical
+# Int
+age = 27
+print(type(age))
+
+# Float
+y = 64.5
+print(type(y))
+
+# Complex
+x = 15j
+print(type(x))
+
+##### Sequence Types
+# List
+z = ["nour", "farida"]
+print(type(z))
+
+# tuple
+c = ("nour", 29, 15)
+print(type(c))
+
+# set
+w = {"nour", 29, "farida"}
+print(w)
+print(type(w))
+
+### Mapping Variable
+
+d = {"name":"nour", "age":29}
+print(type(d))
+print(d["name"])
+
+##### Boolean
+g = 5 > 7
+print(g)
+print(5 > 7)
+print(5 == 7)
+print(5 < 7)
